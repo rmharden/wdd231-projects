b2370dc7d5d90179bc5eac9e0bef35bee2a40edc
rmharden <har22079@byui.edu>
2026-09-25 06:47:58 -0400

    followed through the instructions for step 4

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,15 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+const disclaimer = document.querySelector(".disclaimer > a");
+disclaimer.href = parkData.url;
+disclaimer.innerHTMl = parkData.fullName;
+
+function parkInfoTemplate(info) {
+    return `<a href='/' class="park-title">${info.name}</a>
+    <p class="park-details">
+        <span>${info.designation}</span>
+        <span>${info.states}</span>
+    </p>`;
+}
