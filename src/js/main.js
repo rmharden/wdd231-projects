86716219d47467fa055b848a0a0e0b9374218b7d
rmharden <har22079@byui.edu>
2026-09-25 08:34:38 -0400

    had a mistake with the l in innerhtml and fixed it.

--- src/js/main.js
+++ src/js/main.js
@@ -14,7 +14,7 @@ function parkInfoTemplate(info) {
 
 const disclaimer = document.querySelector(".disclaimer > a");
 disclaimer.href = parkData.url;
-disclaimer.innerHTMl = parkData.fullName;
+disclaimer.innerHTML = parkData.fullName;
 document.querySelector("head > title").textContent = parkData.fullName;
 document.querySelector("park-container > img").src = parkData.images[0].url;
 document.querySelector(".park_content").innerHTML = parkInfoTemplate(parkData);
